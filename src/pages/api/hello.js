030d50cd87d696e54ca80da3e5c3c3fba81ae483
Dipankar Maikap <45673791+dipankarmaikap@users.noreply.github.com>
2022-01-24 16:58:17 +0530

    Added console log for check

--- src/pages/api/hello.js
+++ src/pages/api/hello.js
@@ -1,5 +1,6 @@
 export default function Hello(req, res) {
   let { body } = req;
   const ip = req.headers["x-real-ip"] || req.connection.remoteAddress;
+  console.log({ body, ip });
   res.status(200).json({ body, ip });
 }
